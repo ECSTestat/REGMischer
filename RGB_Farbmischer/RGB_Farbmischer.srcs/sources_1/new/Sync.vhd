9d62f5e1a99664fae3539e46cd3129785b440129
DESKTOP-7EVIT25\mario <mario.vonfluee@stud.hslu.ch>
2018-04-28 14:22:46 +0200

    @Sync > Switches and Encoder synchronized

---
+++ RGB_Farbmischer/RGB_Farbmischer.srcs/sources_1/new/Sync.vhd
@@ -0,0 +1,79 @@
+-------------------------------------------------------------------------------
+-- Entity: Sync
+-- Author: von Flüe
+-------------------------------------------------------------------------------
+-- Description: (ECS Testat 1)
+-- Synchronization and Debouncing for "RGB Farbmischer"
+-------------------------------------------------------------------------------
+-- Total # of FFs: 
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity Sync is
+  generic(
+    CLK_FRQ : integer := 125_000_000 -- 125 MHz
+    );
+  port(
+  rst_pi       : in  std_logic;
+  clk_pi       : in  std_logic;
+  enc_pi       : in  std_logic_vector(1 downto 0);                    
+  enc_sync_po  : out std_logic_vector(1 downto 0);
+  act_pi       : in  std_logic_vector(2 downto 0);
+  act_sync_po  : out std_logic_vector(2 downto 0)
+    );
+end Sync;
+
+architecture rtl of Sync is
+
+  -- synchronization signals
+  signal enca_reg : std_logic_vector(1 downto 0); 
+  signal encb_reg : std_logic_vector(1 downto 0);    
+  signal actsw0_reg  : std_logic_vector(1 downto 0); 
+  signal actsw1_reg  : std_logic_vector(1 downto 0);
+  signal actsw2_reg  : std_logic_vector(1 downto 0);      
+
+begin
+
+  -- output assignments
+  enc_sync_po(0)   <= enca_reg(1);
+  enc_sync_po(1)   <= encb_reg(1);
+  act_sync_po(0)   <= actsw0_reg(1);  
+  act_sync_po(1)   <= actsw1_reg(1); 
+  act_sync_po(2)   <= actsw2_reg(1);       
+  
+  ----------------------------------------------------------------------------- 
+  -- sequential process: synchronization
+  -- All switch inputs and encoder are synchronized. The SW input is assumed to be a
+  -- constant (vector) input.
+  -- # of FF: 
+  ----------------------------------------------------------------------------- 
+  P_syn: process(rst_pi, clk_pi)
+  begin
+    if rst_pi = '1' then
+      enca_reg <= (others => '0');
+      encb_reg  <= (others => '0');
+      actsw0_reg <= (others => '0');
+      actsw1_reg <= (others => '0');
+      actsw2_reg <= (others => '0');
+    elsif rising_edge(clk_pi) then
+      -- synchronization of the encoder A
+      enca_reg(0) <= enc_pi(0);
+      enca_reg(1) <= enca_reg(0);
+      -- synchronization of the encoder B
+      encb_reg(0) <= enc_pi(1);
+      encb_reg(1) <= encb_reg(0);
+      -- synchronization of switch 0
+      actsw0_reg(0) <= act_pi(0);
+      actsw0_reg(1) <= actsw0_reg(0);
+      -- synchronization of switch 1
+      actsw1_reg(0) <= act_pi(1);
+      actsw1_reg(1) <= actsw1_reg(0);
+      -- synchronization of switch 2
+      actsw2_reg(0) <= act_pi(2);
+      actsw2_reg(1) <= actsw2_reg(0);
+    end if;
+  end process;
+end rtl;
